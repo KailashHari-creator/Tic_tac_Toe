fa3e291dc2d3d40cca1a577edaf4feb9f8ca9ee0
Lord_Krishna <thatsmekailash@gmail.com>
2025-07-17 07:25:15 +0530

    Deploy -p1

--- server/db.js
+++ server/db.js
@@ -10,6 +10,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false, // Set to true if you want to enforce SSL certificate validation
+  },
 });
 
 export default pool;
